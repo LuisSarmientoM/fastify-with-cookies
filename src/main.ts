2bcf422669d0c74cd3c6ed7424b9b55f067acd43
Luis Sarmiento <lsarmientom010@gmail.com>
2023-09-13 20:17:18 -0500

    cookies

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import fastifyCookie from '@fastify/cookie';
+import helmet from '@fastify/helmet';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  //   If remove async, it will throw error like this: Plugin did not start in time: '() => cookie_1.default'.
+  await app.register(async () => fastifyCookie, {
+    secret: 'my-secret',
+  });
+  await app.register(helmet);
+  await app.listen(3000);
+}
+bootstrap();
